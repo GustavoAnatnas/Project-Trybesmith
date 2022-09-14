b7ed5b431d1b84aad62ff63e514c7e9a47fcd69b
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-09-13 23:40:02 -0300

    Cria função create responsável por cadastrar novos produtos

--- src/models/productsModel.ts
+++ src/models/productsModel.ts
@@ -1,6 +1,6 @@
-import { RowDataPacket } from 'mysql2/promise';   
+import { RowDataPacket, ResultSetHeader } from 'mysql2/promise';   
 import connection from './connection';
-import { Product } from '../types/products';
+import { Product, ProductBody } from '../types/products';
 
 type Products = Product & RowDataPacket;
 
@@ -9,6 +9,15 @@ const getAll = async (): Promise<Product[]> => {
   return rows;
 };
 
+const create = async (product: ProductBody): Promise<Product> => {
+  const [rows] = await connection.execute<ResultSetHeader>(
+    'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+    [product.name, product.amount],
+  );
+  return { id: rows.insertId, ...product };
+};
+
 export default {
   getAll,
+  create,
 };
