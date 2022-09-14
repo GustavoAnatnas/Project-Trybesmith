2ba92242a98ec42701e13b7caf07c31b40bcec4a
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-09-13 23:43:27 -0300

    Implementa Create na camada de Controller e cria rota Post de produtos

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express';
 import productsServices from '../services/productsServices';
-// import { Product } from '../types/products';
 
 const getAll = async (_req: Request, res: Response): Promise<void> => {
   const products = await productsServices.getAll();
   res.status(200).json(products);
 };
 
+const create = async (req: Request, res: Response): Promise<void> => {
+  const { body } = req;
+  const product = await productsServices.create(body);
+  res.status(201).json(product);
+};
+
 export default {
   getAll,
+  create,
 };
