83061b42f606c14f9b8f6c5602b1eaa58a0cebdd
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-09-13 23:42:45 -0300

    Implementa Create na camada services

--- src/services/productsServices.ts
+++ src/services/productsServices.ts
@@ -1,11 +1,17 @@
 import productsModel from '../models/productsModel';
-import { Product } from '../types/products';
+import { Product, ProductBody } from '../types/products';
 
 const getAll = async (): Promise<Product[]> => {
   const products = await productsModel.getAll();
   return products;
 };
 
+const create = async (newProduct: ProductBody): Promise<Product> => {
+  const product = await productsModel.create(newProduct);
+  return product;
+};
+
 export default {
   getAll,
+  create,
 };
